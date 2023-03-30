8e60c90e163ea5bfc34dbdf3082745fc4df89e54
Bayram Ali Degirmenci <bayramalidegirmenci0@gmail.com>
2023-03-30 22:27:46 +0300

    PizzaciForm
    Pizza siparişi için form ekranı

---
+++ Pizzacı-form/Pizzacı-form/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pizzacı_form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            listBox2.Items.Add(textBox2.Text);
+            listBox3.Items.Add(textBox3.Text);
+            listBox4.Items.Add(comboBox1.Text);
+            listBox5.Items.Add(comboBox2.Text);
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox6.Items.Add(checkBox1.Text);
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox6.Items.Add(checkBox3.Text);
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox6.Items.Add(checkBox4.Text);
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox6.Items.Add(checkBox5.Text);
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox6.Items.Add(checkBox4.Text);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            listBox6.Items.Add(checkBox2.Text);
+        }
+    }
+}
